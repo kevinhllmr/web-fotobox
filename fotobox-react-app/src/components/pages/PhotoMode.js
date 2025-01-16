76470ae29017e0e8f4286f9e680a712f7cfe4933
Cardostyle <cardostyle19@gmail.com>
2025-01-16 19:21:17 +0100

    Photomode cleaned except RTC functions

--- fotobox-react-app/src/components/pages/PhotoMode.js
+++ fotobox-react-app/src/components/pages/PhotoMode.js
@@ -11,10 +11,8 @@ function PhotoMode() {
   const navigate = useNavigate();
   const [camera, setCamera] = useState(null);
   const [cameraActive, setCameraActive] = useState(false);
-  const [device, setDevice] = useState(null);
   const [imageSrc, setImageSrc] = useState(null);
   const [photoTaken, setPhotoTaken] = useState(false);
-  const [webrtcConnected, setWebrtcConnected] = useState(false);
   const [showButtons, setButtonsShown] = useState(false);
   const [timerValue, setTimerValue] = useState(3);
   const [countdown, setCountdown] = useState(0);
@@ -23,11 +21,14 @@ function PhotoMode() {
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
 
+  const [webrtcConnected, setWebrtcConnected] = useState(false);
+  const CHUNK_SIZE = 16000; // 16kB per chunk
+
 
   useEffect(() => {
     const handleUnload = async () => {
       console.log("Seite wird neugeladen, Kamera wird getrennt.");
-      await cleanupResources();
+      await cleanupResourcesGlobal();
     };
 
     window.addEventListener("beforeunload", handleUnload);
@@ -35,6 +36,27 @@ function PhotoMode() {
     const deviceUsed = localStorage.getItem('deviceUsed');
     if (deviceUsed === 'phone') {
       setButtonsShown(false);
+    }
+
+    const initializeCameraIntern = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    
+        if (videoRef.current) {
+          videoRef.current.pause(); // Stoppe das aktuelle Video, falls es läuft
+          videoRef.current.srcObject = null; // Entferne die aktuelle Quelle
+        }
+    
+        videoRef.current.srcObject = stream;
+        await videoRef.current.play(); // Starte das Video
+        setCameraActive(true);
+      } catch (error) {
+        console.error("Fehler beim Zugriff auf die Kamera:", error);
+      }
+    };
+
+    if (!Peripherie.hasExternCamera) {
+      initializeCameraIntern();
     }
 
     const checkWebRTCConnection = () => {
@@ -57,27 +79,6 @@ function PhotoMode() {
     WebRTC.onData(handleIncomingData);
 
 
-    const initializeInternalCamera = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-    
-        if (videoRef.current) {
-          videoRef.current.pause(); // Stoppe das aktuelle Video, falls es läuft
-          videoRef.current.srcObject = null; // Entferne die aktuelle Quelle
-        }
-    
-        videoRef.current.srcObject = stream;
-        await videoRef.current.play(); // Starte das Video
-        setCameraActive(true);
-      } catch (error) {
-        console.error("Fehler beim Zugriff auf die Kamera:", error);
-      }
-    };
-
-    if (!Peripherie.hasExternCamera) {
-      initializeInternalCamera();
-    }
-
     return () => {
       window.removeEventListener("beforeunload", handleUnload);
       if (videoRef.current && videoRef.current.srcObject) {
@@ -87,83 +88,25 @@ function PhotoMode() {
     };
   }, [camera, streamIntervalId]);
 
-  const cleanupResources = async () => {
-    if (streamIntervalId) {
-      clearInterval(streamIntervalId);
-      setStreamIntervalId(null);
-      console.log("Video-Stream gestoppt.");
-    }
-    if (camera) {
-      try {
-        await camera.disconnect();
-        console.log("Kamera erfolgreich getrennt.");
-        setCamera(null);
-      } catch (error) {
-        console.error("Fehler beim Trennen der Kamera:", error);
-      }
-    }
-  };
 
-  const initializeCamera = async () => {
-    try {
-      console.log("Initializing camera...");
-      const newCamera = new Camera();
-      await Camera.showPicker(); // Zeige den Picker
-      await newCamera.connect(); // Verbinde mit der Kamera
-      setCamera(newCamera);
-
-      console.log("Operations supported by the camera:", await newCamera.getSupportedOps());
-      console.log("Current configuration tree:", await newCamera.getConfig());
-
-      // Vorschau starten
-      setTimeout(async () => {
-        try {
-          const blob = await newCamera.capturePreviewAsBlob();
-          const imgURL = URL.createObjectURL(blob);
-          setBlobURL(imgURL);
-          setCameraActive(true);
-          setButtonsShown(true);
-          startVideoStream(newCamera);
-        } catch (error) {
-          console.error("Fehler beim Abrufen der Kamera-Vorschau:", error);
-        }
-      }, 1000);
-    } catch (error) {
-      console.error("Fehler bei der Kamera-Initialisierung:", error);
-    }
-  };
-
-  const startVideoStream = (camera) => {
-    const intervalId = setInterval(async () => {
-      try {
-        const blob = await camera.capturePreviewAsBlob();
-        const imgURL = URL.createObjectURL(blob);
-        setBlobURL(imgURL);
-      } catch (error) {
-        console.error("Fehler beim Abrufen des Kamera-Streams:", error);
-        clearInterval(intervalId);
-        setStreamIntervalId(null);
-      }
-    }, 50);
-    setStreamIntervalId(intervalId); // Speichere die Intervall-ID
-  };
-
-  const handleStartApp = () => {
-    console.log("Starting camera app...");
-    initializeCamera();
-  };
+/*WEB RTC FUNCTIONS */
+/*****************************************************************/
+/*****************************************************************/
+/*****************************************************************/
+/*****************************************************************/
+/*****************************************************************/
 
   const handleIncomingData = (data) => {
     const message = JSON.parse(data);
     switch (message.type) {
       case 'startCountdown':
-        startPhotoCountdown();
+        startPhotoCountdownExtern();
         break;
       case 'savePhoto':
-        handleSavePicture();
+        uploadImageToCloud(imageSrc);
         break;
       case 'retryPhoto':
-        handleRetakePicture();
+        handleRetakePictureExtern();
         break;
       case 'photoData':
         handleReceivedPhotoData(message.data);
@@ -172,65 +115,6 @@ function PhotoMode() {
         break;
     }
   };
-
-  const handleRetakePicture = () => {
-    setImageSrc(null);
-    setPhotoTaken(false);
-    setCameraActive(false);
-    setButtonsShown(true);
-    setTimeout(() => {
-      setCameraActive(true);
-    }, 1);
-  };
-
-  const handleDeleteLastPhoto = async () => {
-    console.log("Foto verwerfen...");
-    await deleteLastImageFromIndexedDB();
-    setImageSrc(null); // Bildquelle zurücksetzen
-    setPhotoTaken(false); // Zustand zurücksetzen
-    setButtonsShown(true); // Zeige die Buttons wieder an
-  };
-
-  const handleEndSession = async () => {
-    console.log("Seite wird verlassen, Kamera wird getrennt.");
-    await cleanupResources();
-    navigate("/home/");
-    window.location.reload();
-  };
-
-  const startPhotoCountdown = () => {
-    console.log("Starting photo countdown...");
-    setButtonsShown(false);
-    startCountdown(timerValue, setCountdown, capturePicture);
-  };
-
-  const handleStartCountdown = () => {
-    startCountdown(3, setCountdown, () => {
-      takePicture(videoRef, canvasRef, setImageSrc, setPhotoTaken);
-    });
-  };
-
-  const capturePicture = async () => {
-    if (!camera) {
-      console.error("Kamera ist nicht verbunden.");
-      return;
-    }
-
-    try {
-      console.log("Taking picture...");
-      const file = await camera.captureImageAsFile();
-      saveImageToIndexedDB(file);
-      const imgURL = URL.createObjectURL(file);
-      setImageSrc(imgURL);
-
-      setBlobURL(null);
-      setPhotoTaken(true);
-    } catch (error) {
-      console.error("Fehler beim Aufnehmen des Bildes:", error);
-    }
-  };
-
-  const CHUNK_SIZE = 16000; // 16kB per chunk
 
   const sendPhotoData = (photoDataUrl) => {
     if (WebRTC.dataChannel && WebRTC.dataChannel.readyState === 'open') {
@@ -260,23 +144,25 @@ function PhotoMode() {
     return chunks;
   };
 
-
-  const handleSavePicture = () => {
-    if (Peripherie.cloudAccess) {
-      uploadImageToCloud(imageSrc);
-    } else {
-      downloadImage(imageSrc);
-    }
+  const handleReceivedPhotoData = (photoData) => {
+    const link = document.createElement('a');
+    link.href = photoData;
+    link.download = 'photo.png';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   };
 
-  const handleSavePhoto = async () => {
-    if (imageSrc) {
-      const blob = await fetch(imageSrc).then((res) => res.blob());
-      saveImageToIndexedDB(blob);
-      handleRetakeIntern();
-    }
-  };
 
+/*Photomode FUNCTIONS EXTERN/INTERN */
+/*****************************************************************/
+/*****************************************************************/
+/*****************************************************************/
+/*****************************************************************/
+/*****************************************************************/
+
+  /*Intern Functions*/
+  /*****************************************************************/
   const handleRetakeIntern=async () => {
     setImageSrc(null);
     setPhotoTaken(false);
@@ -297,13 +183,143 @@ function PhotoMode() {
     }
   }
 
-  const handleReceivedPhotoData = (photoData) => {
-    const link = document.createElement('a');
-    link.href = photoData;
-    link.download = 'photo.png';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  const handleSavePhotoIntern = async () => {
+    if (imageSrc) {
+      const blob = await fetch(imageSrc).then((res) => res.blob());
+      saveImageToIndexedDB(blob);
+      handleRetakeIntern();
+    }
+  };
+
+  const handleStartCountdownIntern = () => {
+    startCountdown(3, setCountdown, () => {
+      takePicture(videoRef, canvasRef, setImageSrc, setPhotoTaken);
+    });
+  };
+
+
+  /*Extern Functions*/
+  /*****************************************************************/
+  const handleRetakePictureExtern = () => {
+    setImageSrc(null);
+    setPhotoTaken(false);
+    setCameraActive(false);
+    setButtonsShown(true);
+    setTimeout(() => {
+      setCameraActive(true);
+    }, 1);
+  };
+
+  const handleDeleteLastPhotoExtern = async () => {
+    console.log("Foto verwerfen...");
+    await deleteLastImageFromIndexedDB();
+    setImageSrc(null); // Bildquelle zurücksetzen
+    setPhotoTaken(false); // Zustand zurücksetzen
+    setButtonsShown(true); // Zeige die Buttons wieder an
+  };
+
+
+
+  const startPhotoCountdownExtern = () => {
+    console.log("Starting photo countdown...");
+    setButtonsShown(false);
+    startCountdown(timerValue, setCountdown, capturePictureExtern);
+  };
+
+
+  const capturePictureExtern = async () => {
+    if (!camera) {
+      console.error("Kamera ist nicht verbunden.");
+      return;
+    }
+
+    try {
+      console.log("Taking picture...");
+      const file = await camera.captureImageAsFile();
+      saveImageToIndexedDB(file);
+      const imgURL = URL.createObjectURL(file);
+      setImageSrc(imgURL);
+
+      setBlobURL(null);
+      setPhotoTaken(true);
+    } catch (error) {
+      console.error("Fehler beim Aufnehmen des Bildes:", error);
+    }
+  };
+
+  const handleStartAppExtern = () => {
+    console.log("Starting camera app...");
+    initializeCameraExtern();
+  };
+
+  const initializeCameraExtern = async () => {
+    try {
+      console.log("Initializing camera...");
+      const newCamera = new Camera();
+      await Camera.showPicker(); // Zeige den Picker
+      await newCamera.connect(); // Verbinde mit der Kamera
+      setCamera(newCamera);
+
+      console.log("Operations supported by the camera:", await newCamera.getSupportedOps());
+      console.log("Current configuration tree:", await newCamera.getConfig());
+
+      // Vorschau starten
+      setTimeout(async () => {
+        try {
+          const blob = await newCamera.capturePreviewAsBlob();
+          const imgURL = URL.createObjectURL(blob);
+          setBlobURL(imgURL);
+          setCameraActive(true);
+          setButtonsShown(true);
+          startVideoStreamExtern(newCamera);
+        } catch (error) {
+          console.error("Fehler beim Abrufen der Kamera-Vorschau:", error);
+        }
+      }, 1000);
+    } catch (error) {
+      console.error("Fehler bei der Kamera-Initialisierung:", error);
+    }
+  };
+
+  const startVideoStreamExtern = (camera) => {
+    const intervalId = setInterval(async () => {
+      try {
+        const blob = await camera.capturePreviewAsBlob();
+        const imgURL = URL.createObjectURL(blob);
+        setBlobURL(imgURL);
+      } catch (error) {
+        console.error("Fehler beim Abrufen des Kamera-Streams:", error);
+        clearInterval(intervalId);
+        setStreamIntervalId(null);
+      }
+    }, 50);
+    setStreamIntervalId(intervalId); // Speichere die Intervall-ID
+  };
+
+  /*Global Functions*/
+  /*****************************************************************/
+  const handleEndSessionGlobal = async () => {
+    console.log("Seite wird verlassen, Kamera wird getrennt.");
+    await cleanupResourcesGlobal();
+    navigate("/home/");
+    window.location.reload();
+  };
+
+  const cleanupResourcesGlobal = async () => {
+    if (streamIntervalId) {
+      clearInterval(streamIntervalId);
+      setStreamIntervalId(null);
+      console.log("Video-Stream gestoppt.");
+    }
+    if (camera) {
+      try {
+        await camera.disconnect();
+        console.log("Kamera erfolgreich getrennt.");
+        setCamera(null);
+      } catch (error) {
+        console.error("Fehler beim Trennen der Kamera:", error);
+      }
+    }
   };
 
   return (
@@ -317,7 +333,7 @@ function PhotoMode() {
         {Peripherie.hasExternCamera ? (
           !cameraActive ? (
             <>
-              <button className="start-button" onClick={handleStartApp}>
+              <button className="start-button" onClick={handleStartAppExtern}>
                 Start
               </button>
               <div className="instructions">
@@ -328,7 +344,7 @@ function PhotoMode() {
                 </ol>
               </div>
               <div className="footer">
-                <p id="back-button" onClick={handleEndSession}>zurück</p>
+                <p id="back-button" onClick={handleEndSessionGlobal}>zurück</p>
               </div>
             </>
           ) : (
@@ -338,10 +354,10 @@ function PhotoMode() {
                   <img src={blobURL} alt="Camera Preview" className="camera-preview" />
                   {showButtons && (
                     <div className="button-container">
-                      <button className="start-button" onClick={startPhotoCountdown}>
+                      <button className="start-button" onClick={startPhotoCountdownExtern}>
                         Starte Countdown
                       </button>
-                      <button className="end-button" onClick={handleEndSession}>
+                      <button className="end-button" onClick={handleEndSessionGlobal}>
                         Sitzung beenden
                       </button>
                     </div>
@@ -352,10 +368,10 @@ function PhotoMode() {
                 <>
                   <img className="camera-preview" src={imageSrc} alt="Captured" />
                   <div className="button-container">
-                    <button className="start-button" onClick={handleRetakePicture}>
+                    <button className="start-button" onClick={handleRetakePictureExtern}>
                       Foto speichern
                     </button>
-                    <button className="end-button" onClick={handleDeleteLastPhoto}>
+                    <button className="end-button" onClick={handleDeleteLastPhotoExtern}>
                       Foto verwerfen
                     </button>
                   </div>
@@ -371,10 +387,10 @@ function PhotoMode() {
                 <canvas ref={canvasRef} style={{ display: "none" }} />
                 {countdown > 0 && <div className="countdown">{countdown}</div>}
                 <div className="button-container">
-                  <button className="start-button" onClick={handleStartCountdown}>
+                  <button className="start-button" onClick={handleStartCountdownIntern}>
                     Starte Countdown
                   </button>
-                  <button className="end-button" onClick={handleEndSession}>
+                  <button className="end-button" onClick={handleEndSessionGlobal}>
                         Sitzung beenden
                       </button>
                 </div>
@@ -383,7 +399,7 @@ function PhotoMode() {
               <>
                 <img className="camera-preview" src={imageSrc} alt="Captured" />
                 <div className="button-container">
-                  <button className="start-button" onClick={handleSavePhoto}>
+                  <button className="start-button" onClick={handleSavePhotoIntern}>
                     Foto speichern
                   </button>
                   <button
